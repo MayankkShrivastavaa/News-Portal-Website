e74ad47967e3f39a8d80e6404dfc9c05a9b5cc7c
MayankkShrivastavaa <mshri252@gmail.com>
2026-08-02 22:11:53 +0530

    feat: added API services & footer comp

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"
 
 
 function App(){
@@ -16,6 +17,7 @@ function App(){
       <Route path="/contact" element={<Contact/>}/>
       <Route path="*" element={<NotFound/>}/>
     </Routes>
+    <Footer/>
     
     </>
   )
